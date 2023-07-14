feec3f9c3273fd279c2231f7fa6d1c8cc5cc7225
Derek <derekdanielson84@gmail.com>
2023-07-14 10:47:34 -0600

    Notes added

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ function createPost(post, cb) {
     }, 2000);
 }
 
+function getPosts() {
+    setTimeout(() => {
+        posts.forEach(function (post) {
+            const div = document.createElement('div');
+            div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+            document.querySelector('#posts').appendChild(div);
+        });
+    }, 1000);
+}
+
+createPost({ title: 'Post Three', body: 'This is post' } , getPosts);
